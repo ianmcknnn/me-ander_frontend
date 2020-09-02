20734aed2aa6b9d154d31a8fd9d7917ef72e5558
ianmcknnn <ianmcnnn@gmail.com>
2020-09-02 12:13:19 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import map from './Map.jpg';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
       </header>
+      <body>
+        <h1 id='site-title'>Map</h1>
+        <img src={map} className="map" alt="map" />
+      </body>
     </div>
   );
 }
